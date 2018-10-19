8e83641508de5c5f4fb8bcfa05d187d3943ed628
Daniel Sava <danielsavas@gmail.com>
2018-10-19 17:39:13 -0300

    Documentação.

--- src/main/java/br/com/savasoft/exemplos/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/br/com/savasoft/exemplos/springbootexample/SpringBootExampleApplication.java
@@ -9,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *  Artigo de Referência:
  *  - https://dzone.com/articles/securing-rest-apis-with-client-certificates?edition=407227&utm_source=Weekly%20Digest&utm_medium=email&utm_campaign=Weekly%20Digest%202018-10-17
  *
+ *
+ *  Como criar certificados com 'openssl'
+ *  - https://blog.pavelsklenar.com/how-to-create-pkcs-12-for-your-application/
+ *
+ *
  */
 
 @SpringBootApplication
